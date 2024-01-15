10eda0cee5eba86629e57c932c1f4db28eeea9f2
OrionBonnie <146794291+OrionBonnie@users.noreply.github.com>
2024-01-15 18:12:05 +0800

    添加新选项

--- select_ops.py
+++ select_ops.py
@@ -3,6 +3,8 @@ print('''
 这里是 PDF 各类转换操作的选择菜单，请选择你想要的操作
 1. PDF 批量转换 Word
 2. PDF 表格提取至 Excel
+3. PDF 图片提取
+## 注意: 如果 out 文件夹内仍存在文件，将会在选择操作被清空，若存在重要文件，请及时移出 ##
 ''')
 choose = input("请选择对应的操作数字: ")
 if choose == "1":
@@ -11,6 +13,9 @@ if choose == "1":
 elif choose == "2":
     import pdf_table2excel
     pdf_table2excel.main()
+elif choose == "3":
+    import pdf_imagout
+    pdf_imagout.main()
 else:
     print("未知操作, 按任意键退出")
     getch()
